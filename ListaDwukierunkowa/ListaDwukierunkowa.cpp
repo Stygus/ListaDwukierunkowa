09f88766028203973eec92ba284876bb8787a2fb
Stygus <kuba.tokarczyk@onet.eu>
2024-11-05 21:55:47 +0100

    Initial commit

---
+++ ListaDwukierunkowa/ListaDwukierunkowa.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "ListaDwukierunkowa.h"
+
+void ListaDwukierunkowa::Dodawanie_napoczatek(int dane) {
+
+	if (ilosc_elementow > 0)
+	{
+		Wezel* morski = new Wezel;
+		poczatek->poprzedni_wezel = morski;
+		poczatek->poprzedni_wezel->nastepny_wezel = poczatek;  // relacje miedzy wezlami
+		poczatek = morski;    // ustawienie poczatku 
+		morski->wartosc = dane;   //ustawienie danych
+
+
+	}
+	else
+	{
+		Wezel* morski = new Wezel;
+		poczatek = morski;   // jedyny wenzel
+		koniec = morski;
+		morski->wartosc = dane;
+	}
+
+	ilosc_elementow++;
+}
---
+++ ListaDwukierunkowa/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include "ListaDwukierunkowa.h"
+
+using namespace std;
+int main()
+{
+
+   
+  
+    ListaDwukierunkowa a;
+    a.Dodawanie_napoczatek(5);
+    a.Dodawanie_nakoniec(6);
+    a.Dodawanie_nakoniec(7);
+    a.Dodawanie_napoczatek(9);
+    a.Wyswietlanie_od_poczatku();
+    cout << endl;
+    a.Wyswietlanie_od_konca();
+
+
+}
